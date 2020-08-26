d2c6848a94973714c9bd0414761f70a6f351dafd
GuerraUTFPR <guerramatheus2@gmail.com>
2020-08-26 13:53:02 -0300

    fim da semana 1

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -1,5 +1,21 @@
 import { Request, Response } from "express";
+import createUser from "./services/CreateUser";
 
 export function helloWorld(request: Request, response: Response) {
+  const user = createUser({
+    email: "a@email.com",
+    password: "124323",
+    techs: [
+      "Node",
+      "React",
+      "Other",
+      {
+        title: "javascript",
+        experience: 1000,
+      },
+    ],
+  });
+
+  console.log(user.email);
   return response.json({ message: "Hello World" });
 }
